2f5da934a9394fb781b00ee8d279258ed169b7cd
Ahmed Mohammed <ahmed@Ahmeds-MacBook-Pro.local>
2023-01-22 02:14:38 -0700

    added hive col order function

--- src/main/scala/com/code/scala/WordCount.scala
+++ src/main/scala/com/code/scala/WordCount.scala
@@ -4,23 +4,15 @@ import org.apache.spark.sql.SparkSession
 
 object WordCount {
 
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
 
-    val spark:SparkSession = SparkSession.builder().master("local[4]").appName("demo").getOrCreate()
+    val spark: SparkSession = SparkSession.builder().master("local[4]").appName("demo").getOrCreate()
     val sc = spark.sparkContext
     //sc.setCheckpointDir("")
-
     //  import spark.sqlContext.implicits._
-
     sc.setLogLevel("ERROR")
-
     //word count example
-    val text=sc.textFile("E:\\spark_examples\\words.txt")
-
-    text.flatMap(x=>x.split(" ")).filter(x=>x.equalsIgnoreCase("fathima")).map(y=>(y,1)).reduceByKey(_+_).sortByKey().collect().foreach(println)   //one line computation for word count
-
-
-
+    val text = sc.textFile("E:\\spark_examples\\words.txt")
+    text.flatMap(x => x.split(" ")).filter(x => x.equalsIgnoreCase("fathima")).map(y => (y, 1)).reduceByKey(_ + _).sortByKey().collect().foreach(println) //one line computation for word count
   }
-
 }
